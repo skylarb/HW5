8a3e881685743ac231c2217e3958ebe225e86c47
svivo2 <30248162+svivo2@users.noreply.github.com>
2018-08-01 21:29:57 -0500

    Added Skylar's role

--- roles.cpp
+++ roles.cpp
@@ -3,6 +3,11 @@ Function roles
 Tomas Stevens
 
 Skylar B
+created Licence
+Edited Delete Function
+Removed the debugging text
+added the random level generator
+added the insert function
 
 Natalie Bostwick
 
